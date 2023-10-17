26f45faa7fbf185af4ca0e60fd576fe1d2f4f911
adenilsonjunior01 <adenilsonn.souza@gmail.com>
2023-10-17 08:26:14 -0300

    feat: remove mock

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  api: 'https://v3.football.api-sports.io',
+  apiKey: 'aae8f15ebbcddd2e58454dc99331aabb',
+  apiHost: 'v3.football.api-sports.io',
+  production: true,
+};
